247b435f1afe772a0eb31edd5527e292c08c4bc7
unknown <zetlas1610@gmail.com>
2019-12-04 23:22:39 +0100

    Modification du bot

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const { Client } = require("discord.js");
 const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableeveryone: true });
 
-client.on("ready", () => {
-  console.log("Je suis lancer !");
+client.on("message", msg => {
+  if (msg.author.bot) return;
+  const args = msg.content.split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd === `${PREFIX}ping`) msg.channel.send("Pong!");
 });
 
-client.on("message", msg => {
-  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+client.on("guildMemberAdd", member => {
+  member.send("Salut a toi!");
+  const channel = client.channels.find(r => r.name === "general");
+  channel.send(`${member} a rejoint le serveur !`);
 });
 
 client.login("TOKEN");
+
+// ------------------------------------------
+
+client.on("ready", () => console.log("Je suis lancer !"));
+client.on("error", console.error);
+client.on("warn", console.warn);
+client.on("debug", console.debug);
