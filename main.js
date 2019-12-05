e7298d12a6b420bb3b3e754a483a5daf1d48fbb6
unknown <zetlas1610@gmail.com>
2019-12-05 19:46:33 +0100

    Ajout de role avec une commande

--- main.js
+++ main.js
@@ -7,6 +7,24 @@ client.on("message", msg => {
   const args = msg.content.split(/ +/g);
   const cmd = args.shift().toLowerCase();
   if (cmd === `${PREFIX}ping`) msg.channel.send("Pong!");
+  if (cmd === `${PREFIX}repeat`) {
+    msg.channel.send(args.join(" "));
+    msg.delete({ timeout: 3000 }).then(console.log("Message supprimé"));
+  }
+  if (cmd === `${PREFIX}role`) {
+    const channel = client.channels.find(r => r.name === "logs");
+    const role = msg.guild.role.find(r => r.name === args[0]);
+    if (!role) return msg.channel.send("Ce role n'existe pas !");
+    if (msg.member.roles.find(r => r.name === args[0])) {
+      msg.member.roles.remove(role);
+      channel.send(`J'ai supprimé le role ${role} a ${msg.author}.`);
+      msg.delete({ timeout: 3000 });
+    } else {
+      msg.member.roles.add(role);
+      channel.send(`J'ai ajouter le role ${role} a ${msg.author}.`);
+      msg.delete({ timeout: 3000 });
+    }
+  }
 });
 
 client.on("guildMemberAdd", member => {
